3d54ef78f1c19188587f6686727be8a9a1f51078
Prakash Rai <prr0154@my.londonmet.ac.uk>
2021-02-11 19:13:02 +0545

    second commit.tempApp component and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import TempApp from "./Components/TempApp";
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <TempApp />
+    </>
+  );
+};
 
 export default App;
---
+++ src/Components/TempApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TempApp = () => {
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input type="search" className="inputField" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TempApp;
