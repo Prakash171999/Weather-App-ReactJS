755a8cc55b9872979e2e204bb6b50999faa28d18
Prakash Rai <prr0154@my.londonmet.ac.uk>
2021-02-17 00:19:03 +0545

    few changes. fetching api. Still few bugs

--- src/Components/TempApp.js
+++ src/Components/TempApp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import helpers from "./Helpers";
-
 
 const api = {
   key: helpers.API_KEY,
@@ -8,31 +8,41 @@ const api = {
 };
 
 const TempApp = () => {
-  
   const [weather, setWeather] = useState({});
   const [query, setQuery] = useState("");
 
-  //fetching data from openweather api
   useEffect(() => {
-    const fetchApi = async () => {
-      const url = `${api.base}weather?q=${query}&appid=${api.key}`;
-      const response = await fetch(url);
-      const resJson = await response.json();
-      console.log(resJson);
-      setWeather(resJson);
-    };
-    fetchApi();
-  }, [query]);
+    search();
+  }, [query])
+
+  const search = (event) => {
+    let url = `${api.base}weather?q=${query}&units=metric&appid=${api.key}`;
+    // if (event.key === "Enter") {
+      fetch(url)
+        .then((res) => res.json())
+        .then((apiData) => {
+          setWeather(apiData);
+          console.log(apiData);
+        });
+    //}
+  };
 
   const handleInput = (event) => {
-    setQuery(event.target.value)
+    setQuery(event.target.value);
   };
 
   return (
     <>
       <div className="main">
         <div className="inputData">
-          <input type="search" placeholder="Search...." className="inputField" onChange={handleInput} />
+          <input
+            type="search"
+            placeholder="Search...."
+            className="inputField"
+            onChange={handleInput}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
         {!query ? (
           <div className="noData">
@@ -41,11 +51,28 @@ const TempApp = () => {
         ) : (
           <div>
             <div className="info">
-              <h2 className="location">Kathmandu, Nepal</h2>
+              <h2 className="title">Weather Condition</h2>
+              {weather.temp > 26 ?
+              (<img
+              src="/images/hot.png"
+              alt="weather-condition"
+              className="weather-condition"
+            />) : (
+              <img
+                src="/images/mild.png"
+                alt="weather-condition"
+                className="weather-condition"
+              />
+            )
+              }
+              
+
+              <h2 className="location">
+               {weather.name}
+              </h2>
               <h2 className="date">Tuesday, 2021</h2>
-              <img src="/images/hot.png" alt="weather-condition" className="weather-condition" />
-              <h2 className="temp">26 C</h2>
-              <h3 className="tempmin_max">Min : 5.25Cel | Max : 5.25 Cel </h3>
+              <h2 className="temp"> {weather.main.temp}°Cel</h2>
+              <h3 className="tempmin_max">Min: {weather.main.temp_min} °Cel | Max: {weather.main.temp_max}°Cel</h3>
             </div>
           </div>
         )}
